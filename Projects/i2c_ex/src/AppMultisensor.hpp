90bca30bc54b9e58b8ef9ca105717420704842c8
Djordje Dimitrov <dimitrovdjordje@gmail.com>
2024-10-21 17:56:56 +0200

    Finally bpm sensor works .

--- Projects/i2c_ex/src/AppMultisensor.hpp
+++ Projects/i2c_ex/src/AppMultisensor.hpp
@@ -1,32 +1,45 @@
 #pragma once
 
-#include <tsl2591.h>
 #include <bmp280.h>
+#include <cstring>
 
-namespace app{
-    struct SensorReading
-    {
-        float pressure;float temperature;float humidity;float lux;
-        /* data */
+namespace app {
+    struct SensorReading {
+        float pressure;
+        float temperature;
+        float humidity;
+        // float lux;  // Commented out as we are not using the light sensor
     };
-    
-    class AppMultisensor
-    {
+
+    class AppMultisensor {
     private:
-        tsl2591_t m_light_sensor;
+        // tsl2591_t m_light_sensor;  // Commented out as we are not using the light sensor
         bmp280_t m_temp_sensor;
         bmp280_params_t m_bmp280_params;
+
     public:
-        AppMultisensor(/* args */);
-        ~AppMultisensor();
+        void init(void) {
+            ESP_ERROR_CHECK(i2cdev_init());
+            // Commented out the TSL2591 initialization
+            /*
+            memset(&m_light_sensor, 0, sizeof(tsl2591_t));
+            ESP_ERROR_CHECK(tsl2591_init_desc(&m_light_sensor, I2C_NUM_1,
+                                              GPIO_NUM_41, GPIO_NUM_40));
+            ESP_ERROR_CHECK(tsl2591_init(&m_light_sensor));
+            */
+            memset(&m_temp_sensor, 0, sizeof(bmp280_t));
+            ESP_ERROR_CHECK(bmp280_init_default_params(&m_bmp280_params));
+            ESP_ERROR_CHECK(bmp280_init_desc(&m_temp_sensor,
+                                             BMP280_I2C_ADDRESS_1, I2C_NUM_1,
+                                             GPIO_NUM_41, GPIO_NUM_40));
+            ESP_ERROR_CHECK(bmp280_init(&m_temp_sensor, &m_bmp280_params));
+        }
+
+        SensorReading read(void) {
+            SensorReading reading;
+            bmp280_read_float(&m_temp_sensor, &reading.temperature, &reading.pressure, &reading.humidity);
+            // tsl2591_get_lux(&m_light_sensor, &reading.lux);  // Commented out as we are not using the light sensor
+            return reading;
+        }
     };
-    
-    AppMultisensor::AppMultisensor(/* args */)
-    {
-    }
-    
-    AppMultisensor::~AppMultisensor()
-    {
-    }
-    
 }
--- Projects/i2c_ex/src/i2c_ex.cpp
+++ Projects/i2c_ex/src/i2c_ex.cpp
@@ -1,7 +1,26 @@
-void app_main() {
-    while (true)
-    {
-        /* code */
+#include "esp_err.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "AppMultisensor.hpp"
+
+extern "C" void app_main(void) {
+    // Delay for 10 seconds (or adjust as needed) before starting
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+    printf("Waiting for 10 seconds before starting AppMultisensor...\n");
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
+
+    printf("Starting AppMultisensor...\n");
+    app::AppMultisensor multisensor;
+    multisensor.init();
+
+    while (true) {
+        printf("Reading sensor data...\n");
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+        auto reading = multisensor.read();
+
+        ESP_LOGI(__func__, "pres: %.2f, temp: %.2f, hum: %.2f",
+                 reading.pressure, reading.temperature, reading.humidity);
     }
-    
-} 
+}
